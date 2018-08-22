e503ad770ce28a9d3eb1174b8c7044f682b508d2
DuParkLinux <dupark3@gmail.com>
2018-08-22 14:48:49 -0400

    for loop syntax is the same.

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,16 @@
 public class helloworld{
     public static void main(String[] args){
         System.out.println("hello world!");
+        
+        // concatenating strings with overloaded + operator
+        int x = 10, y = 20;
+        System.out.println("x: " + x + "\ny: " + y);
+
+        // for loop has the same syntax as C++
+        int sum = 0;
+        for(int i = 1; i <= 10; ++i) {
+            sum += i;
+        }
+        System.out.println("sum is " + sum);
     }
 }
